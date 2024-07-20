54062219ee483a586cd12a6398c7ad78aedbac02
Enoch <enoch_aik@icloud.com>
2024-07-20 12:20:36 +0200

    Updated app and launcher icons

---
+++ lib/features/onboarding/presentation/ui/screens/onboarding.dart
@@ -0,0 +1,11 @@
+import 'package:dewsclim/lib.dart';
+
+@RoutePage(name: 'onboarding')
+class OnboardingScreen extends HookWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/lib.dart
@@ -0,0 +1,4 @@
+export 'package:flutter/material.dart';
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+export 'package:flutter_hooks/flutter_hooks.dart';
+export 'package:auto_route/auto_route.dart';
