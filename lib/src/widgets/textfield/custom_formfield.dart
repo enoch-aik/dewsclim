49142c4c4a202abe5b4632d6707a0b80cca1c90c
Enoch <enoch_aik@icloud.com>
2024-08-03 07:55:14 +0200

    - added UI for yield calculator - updated label config for CustomFormField

--- lib/src/widgets/textfield/custom_formfield.dart
+++ lib/src/widgets/textfield/custom_formfield.dart
@@ -1,4 +1,5 @@
 import 'package:dewsclim/lib.dart';
+import 'package:dewsclim/src/res/colors/colors.dart';
 import 'package:dewsclim/src/res/styles/styles.dart';
 import 'package:dewsclim/src/widgets/widgets.dart';
 
@@ -7,14 +8,15 @@ class CustomFormField extends StatelessWidget {
   final Widget textField;
   final double? labelFontSize;
   final double? bottomPadding;
+  final Color? labelColor;
 
-  const CustomFormField({
-    super.key,
-    required this.label,
-    required this.textField,
-    this.labelFontSize,
-    this.bottomPadding,
-  });
+  const CustomFormField(
+      {super.key,
+      required this.label,
+      required this.textField,
+      this.labelFontSize,
+      this.bottomPadding,
+      this.labelColor});
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,10 @@ class CustomFormField extends StatelessWidget {
       children: [
         Text(
           label,
-          style: AppStyles.bodyTextStyle.copyWith(fontSize: labelFontSize),
+          style: AppStyles.bodyTextStyle.copyWith(
+              fontSize: labelFontSize,
+              color: labelColor ?? AppColors.neutral800,
+              fontWeight: FontWeight.w400),
         ),
         const ColSpacing(8),
         textField,
